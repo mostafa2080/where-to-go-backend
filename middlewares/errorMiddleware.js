ce6b19fb180703ba7eabad421c9f3cd0454325b7
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-27 03:38:03 +0300

    errorMiddleware Added

---
+++ middlewares/errorMiddleware.js
@@ -0,0 +1,28 @@
+
+
+const sendErrForDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    stack: err.stack,
+    message: err.message,
+  });
+};
+const sendErrForProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
+
+const globalError = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || 'error';
+  if (process.env.NODE_ENV === 'development') {
+    sendErrForDev(err, res);
+  } else {
+    sendErrForProd(err, res);
+  }
+};
+module.exports = globalError;
