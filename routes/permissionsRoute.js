34fff37604b0a4355aa88972acc8d97c0d1585a2
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-30 00:29:58 +0300

    Permission Validator Added and permission Route Refactored

--- routes/permissionsRoute.js
+++ routes/permissionsRoute.js
@@ -8,15 +8,24 @@ const {
   updatePermission,
   deletePermission,
 } = require('../controllers/permissionsController');
+const {
+  validateCreatePermission,
+  validateDeletePermission,
+  validateUpdatePermission,
+  validateGetPermissionById,
+} = require('../utils/validators/PermissionsValidator');
 
 // Routes for '/api/v1/permissions'
-router.route('/').get(getPermissions).post(createPermission);
+router
+  .route('/')
+  .get(getPermissions)
+  .post(validateCreatePermission, createPermission);
 
 // Routes for '/api/v1/permissions/:id'
 router
   .route('/:id')
-  .get(getPermissionById)
-  .put(updatePermission)
-  .delete(deletePermission);
+  .get(validateGetPermissionById, getPermissionById)
+  .put(validateUpdatePermission, updatePermission)
+  .delete(validateDeletePermission, deletePermission);
 
 module.exports = router;
