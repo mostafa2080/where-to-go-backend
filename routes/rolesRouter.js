62fc13a3dd1ba43bae66ad644c9f3533dd4e8075
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-30 04:00:07 +0300

    Roles Route Refactored

--- routes/rolesRouter.js
+++ routes/rolesRouter.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const router = express.Router();
-
 const {
   getRoles,
   getRoleById,
@@ -9,9 +8,21 @@ const {
   updateRole,
   deleteRole,
 } = require('../controllers/rolesController');
+const {
+  validateCreateRole,
+  validateDeleteRole,
+  validateUpdateRole,
+  validateGetRoleById,
+} = require('../utils/validators/rolesValidator');
 
-// Routes
-router.route('/').get(getRoles).post(createRole);
-router.route('/:id').get(getRoleById).put(updateRole).delete(deleteRole);
+// Routes for '/api/v1/roles'
+router.route('/').get(getRoles).post(validateCreateRole, createRole);
+
+// Routes for '/api/v1/roles/:id'
+router
+  .route('/:id')
+  .get(validateGetRoleById, getRoleById)
+  .put(validateUpdateRole, updateRole)
+  .delete(validateDeleteRole, deleteRole);
 
 module.exports = router;
