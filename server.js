b4abd8553d478cd59e5a40d0eaef35afd9ade354
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 22:09:14 +0300

    fixed conflict in server.js

--- server.js
+++ server.js
@@ -1,29 +1,24 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const dbconnection = require("./config/database");
-const ApiError = require("./utils/apiError");
-const globalError = require("./middlewares/errorMiddleware");
-const rolesRoute = require("./routes/rolesRouter");
-const permissionsRoute = require("./routes/permissionsRoute");
-
-// Routes
-const customersRouter = require("./routes/customers");
-
-const vendorsRoute = require("./routes/vendors");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const dbconnection = require('./config/database');
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
 const rolesRoute = require('./routes/rolesRouter');
 const permissionsRoute = require('./routes/permissionsRoute');
-const imagesRouter = require('./routes/imagesRouter');
-const authRouter= require('./routes/authRoute');
+
+// Routes
 const customersRouter = require('./routes/customersRoute');
+
+const vendorsRoute = require('./routes/vendors');
+const imagesRouter = require('./routes/imagesRouter');
+const authRouter = require('./routes/authRoute');
 // const EmployeeRoutes = require('./routes/employee');
 
 //Routes
-// const vendorsRoute = require("./routes/vendorsRoute");
 // const vendorRequestsRoute = require("./routes/vendorRequestsRoute");
 
-dotenv.config({ path: "config.env" });
-
+dotenv.config({ path: 'config.env' });
 
 //express app
 const app = express();
@@ -37,8 +32,8 @@ dbconnection();
 
 //Middleware
 app.use(express.json());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
   console.log(` Mode: ${process.env.NODE_ENV}`);
 }
 
@@ -48,14 +43,12 @@ app.use('/api/v1/roles', rolesRoute);
 app.use('/api/v1/permissions', permissionsRoute);
 app.use('/api/v1/auth', authRouter);
 
-
 // app.use("/api/v1/vendors", vendorsRoute);
 // app.use(EmployeeRoutes);
 
 app.use(imagesRouter);
 
-
-app.all("*", (req, res, next) => {
+app.all('*', (req, res, next) => {
   next(new ApiError(`Can not find this Route ${req.originalUrl}`, 400));
 });
 
@@ -68,7 +61,7 @@ const server = app.listen(PORT, () => {
   console.log(`app running on Port: ${PORT}`);
 });
 
-process.on("unhandledRejection", (err) => {
+process.on('unhandledRejection', (err) => {
   console.error(`UnhandledRejection Error ${err}`);
   server.close(() => {
     console.log(`shutting down....`);
