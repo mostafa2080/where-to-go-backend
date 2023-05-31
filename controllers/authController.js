b576b58d822ecbe061f3b6c3e835f095c91dc1c5
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 18:46:34 +0300

    JWT Configurations Set in config.env file

--- controllers/authController.js
+++ controllers/authController.js
@@ -4,3 +4,8 @@ const asyncHandler = require('express-async-handler');
 const UserModel = require('../models/User');
 const ApiError = require('../utils/apiError');
 
+const createToken = (payload) =>
+  jwt.sign({ payload }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
+
