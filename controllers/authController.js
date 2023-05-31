f1d3714213df30697da5d780ced664918627166e
mostafa2080 <mostafashaaban2080@gmail.com>
2023-05-31 18:43:39 +0300

    eslint modified

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,4 @@
 const crypto = require('crypto');
-// eslint-disable-next-line import/no-unresolved
 const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 const UserModel = require('../models/User');
