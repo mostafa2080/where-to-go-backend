32193f93b171dd17937d465e628479b66dd6b1fe
Osama <0xm.osamaa@gmail.com>
2023-06-06 01:44:49 +0300

    get category by id

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -7,3 +7,8 @@ exports.getAllCategories = async (req, res) => {
     const categories = await Category.find({});
     return res.status(200).json({ status: 'success', data: categories });
 };
+
+exports.getCategory = async (req, res) => {
+    const category = await Category.findOne({ _id: req.params.id });
+    return res.status(200).json({ status: 'success', data: category });
+};
