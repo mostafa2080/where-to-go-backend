44d0c6d7f217d929d6f5572cc6e4a77628c3c576
Osama <0xm.osamaa@gmail.com>
2023-06-06 03:42:42 +0300

    categories and tags routers

--- server.js
+++ server.js
@@ -1,26 +1,28 @@
-const express = require("express");
-const dotenv = require("dotenv");
-const morgan = require("morgan");
-const dbconnection = require("./config/database");
-const ApiError = require("./utils/apiError");
-const globalError = require("./middlewares/errorMiddleware");
-const rolesRoute = require("./routes/rolesRouter");
-const permissionsRoute = require("./routes/permissionsRoute");
+const express = require('express');
+const dotenv = require('dotenv');
+const morgan = require('morgan');
+const dbconnection = require('./config/database');
+const ApiError = require('./utils/apiError');
+const globalError = require('./middlewares/errorMiddleware');
+const rolesRoute = require('./routes/rolesRouter');
+const permissionsRoute = require('./routes/permissionsRoute');
 
 // Routes
-const authRouter = require("./routes/authRoute");
-const customersRouter = require("./routes/customersRoute");
-const vendorsRoute = require("./routes/vendors");
-const imagesRouter = require("./routes/imagesRouter");
+const authRouter = require('./routes/authRoute');
+const customersRouter = require('./routes/customersRoute');
+const vendorsRoute = require('./routes/vendors');
+const imagesRouter = require('./routes/imagesRouter');
+const categoriesRouter = require('./routes/categoriesRouter');
+const tagsRouter = require('./routes/tagsRouter');
 // const EmployeeRoutes = require('./routes/employee');
 
 // Middlewares...
-const authenticationMiddleware = require("./middlewares/authenticationMiddleware");
+const authenticationMiddleware = require('./middlewares/authenticationMiddleware');
 
 //Routes
 // const vendorRequestsRoute = require("./routes/vendorRequestsRoute");
 
-dotenv.config({ path: "config.env" });
+dotenv.config({ path: 'config.env' });
 
 //express app
 const app = express();
@@ -34,26 +36,28 @@ dbconnection();
 
 //Middleware
 app.use(express.json());
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-  console.log(` Mode: ${process.env.NODE_ENV}`);
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+    console.log(` Mode: ${process.env.NODE_ENV}`);
 }
 
 //routes
-app.use("/api/v1/auth", authRouter);
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/categories', categoriesRouter);
+app.use('/api/v1/tags', tagsRouter);
 
 app.use(authenticationMiddleware);
 
 app.use(customersRouter);
-app.use("/api/v1/roles", rolesRoute);
-app.use("/api/v1/permissions", permissionsRoute);
-app.use("/api/v1/", vendorsRoute);
+app.use('/api/v1/roles', rolesRoute);
+app.use('/api/v1/permissions', permissionsRoute);
+app.use('/api/v1/', vendorsRoute);
 // app.use(EmployeeRoutes);
 
 app.use(imagesRouter);
 
-app.all("*", (req, res, next) => {
-  next(new ApiError(`Can not find this Route ${req.originalUrl}`, 400));
+app.all('*', (req, res, next) => {
+    next(new ApiError(`Can not find this Route ${req.originalUrl}`, 400));
 });
 
 //err mw
@@ -62,13 +66,13 @@ app.use(globalError);
 //listening
 const { PORT } = process.env;
 const server = app.listen(PORT, () => {
-  console.log(`app running on Port: ${PORT}`);
+    console.log(`app running on Port: ${PORT}`);
 });
 
-process.on("unhandledRejection", (err) => {
-  console.error(`UnhandledRejection Error ${err}`);
-  server.close(() => {
-    console.log(`shutting down....`);
-    process.exit(1);
-  });
+process.on('unhandledRejection', (err) => {
+    console.error(`UnhandledRejection Error ${err}`);
+    server.close(() => {
+        console.log(`shutting down....`);
+        process.exit(1);
+    });
 });
