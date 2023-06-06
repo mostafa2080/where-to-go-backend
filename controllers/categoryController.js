23d6b751d1328ac3289daa41fce892198d8c30e4
Osama <0xm.osamaa@gmail.com>
2023-06-06 03:11:11 +0300

    category controller crud

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,14 +1,27 @@
-import '../models/Category';
+require('../models/Category');
 const mongoose = require('mongoose');
 
 const Category = mongoose.model('category');
 
-exports.getAllCategories = async (req, res) => {
+exports.getCategories = async (req, res) => {
     const categories = await Category.find({});
     return res.status(200).json({ status: 'success', data: categories });
 };
 
-exports.getCategory = async (req, res) => {
+exports.getCategoryById = async (req, res) => {
     const category = await Category.findOne({ _id: req.params.id });
+    return res.status(200).json({ status: 'success', data: category });
+};
+
+exports.updateCategory = async (req, res) => {
+    const category = await Category.updateOne({ _id: req.params.id }).set({});
+
+    return res.status(200).json({ status: 'success', data: category });
+};
+
+exports.deleteCategory = async (req, res) => {
+    const category = await Category.findOne({ _id: req.params.id });
+    category.delete();
+
     return res.status(200).json({ status: 'success', data: category });
 };
