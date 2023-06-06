2ca7bc1a7a2e5bd568af801d915e436066298610
Osama <0xm.osamaa@gmail.com>
2023-06-06 03:41:25 +0300

    async handler

--- controllers/tagController.js
+++ controllers/tagController.js
@@ -1,29 +1,29 @@
 require('../models/Tag');
 const mongoose = require('mongoose');
-
+const AsyncHandler = require('express-async-handler');
 const ApiError = require('../utils/apiError');
 
 const Tag = mongoose.model('tag');
 
-exports.getTags = async (req, res) => {
+exports.getTags = AsyncHandler(async (req, res) => {
     const tags = await Tag.find({ deletedAt: null });
     if (!tags) return new ApiError('Tags not found!', 404);
     return res.status(200).json({ status: 'success', data: tags });
-};
+});
 
-exports.createTag = async (req, res) => {
+exports.createTag = AsyncHandler(async (req, res) => {
     const tag = await Tag.create({ name: req.body.name });
     if (!tag) return new ApiError('Tag not created.', 400);
     return res.status(200).json({ status: 'success', data: tag });
-};
+});
 
-exports.getTagById = async (req, res) => {
+exports.getTagById = AsyncHandler(async (req, res) => {
     const tag = await Tag.findOne({ _id: req.params.id });
     if (!tag) return new ApiError('Tag not found!', 404);
     return res.status(200).json({ status: 'success', data: tag });
-};
+});
 
-exports.updateTag = async (req, res) => {
+exports.updateTag = AsyncHandler(async (req, res) => {
     const tag = await Tag.findOneAndUpdate({
         _id: req.params.id,
     }).set({
@@ -33,13 +33,13 @@ exports.updateTag = async (req, res) => {
 
     if (!tag) return new ApiError('Tag not found!', 404);
     return res.status(200).json({ status: 'success', data: tag });
-};
+});
 
-exports.deleteTag = async (req, res) => {
+exports.deleteTag = AsyncHandler(async (req, res) => {
     const tag = await Tag.findOneAndUpdate({
         _id: req.params.id,
     }).set({ deletedAt: new Date() });
     if (!tag) return new ApiError('Tag not found!', 404);
 
     return res.status(200).json({ status: 'success', data: tag });
-};
+});
