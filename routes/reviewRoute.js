b0b89e1bdbc40c4b868f4b83e587c9965fa23c46
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-11 13:24:59 +0300

    review route added

---
+++ routes/reviewRoute.js
@@ -0,0 +1,27 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  createReview,
+  getPlaceReviews,
+  updateReview,
+  deleteReview,
+  getReviewById,
+} = require('../controllers/reviewController');
+const {
+  validateCreateReview,
+  validateDeleteReview,
+  validateUpdateReview,
+  validateGetReviewById,
+} = require('../utils/validators/reviewValidator');
+
+router.route('/').post(validateCreateReview, createReview);
+
+router
+  .route('/:id')
+  .get(getPlaceReviews)
+  .put(updateReview)
+  .delete(validateDeleteReview, deleteReview);
+
+module.exports = router;
