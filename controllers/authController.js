929d1b8aad91b48b927e6b10694042d42987476d
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-12 01:23:21 +0300

    minor changes in authcontroller

--- controllers/authController.js
+++ controllers/authController.js
@@ -5,16 +5,16 @@ const jwt = require('jsonwebtoken');
 const asyncHandler = require('express-async-handler');
 const ApiError = require('../utils/apiError');
 const sendMail = require('../utils/sendEmail');
+const customerModel = require('../models/Customer');
 
-require("./../models/Customer");
-require("./../models/Vendor");
-require("./../models/Employee");
-require("./../models/Role");
+require('../models/Customer');
+require('../models/Vendor');
+require('../models/Employee');
+require('../models/Role');
 
-const customerModel = require("../models/Customer");
-const VendorModel = mongoose.model("vendor");
-const EmployeeModel = mongoose.model("employees");
-const RoleModel = mongoose.model("roles");
+const VendorModel = mongoose.model('vendor');
+const EmployeeModel = mongoose.model('employees');
+const RoleModel = mongoose.model('roles');
 
 const saltRunds = 10;
 const salt = bcrypt.genSaltSync(saltRunds);
@@ -30,94 +30,102 @@ const createToken = (payload) =>
 
 // Admins(super admin & employees) Login ...
 exports.adminLogin = asyncHandler(async (req, res, next) => {
-  const employee = await EmployeeModel.findOne({ email: req.body.email});
-  if(!employee) {
-    return next(new ApiError("No such account exists, Try to reqister first...!", 404))
-  }else{
-    if(bcrypt.compareSync(req.body.password, employee.password)){
-      let roleName = ''
+  const employee = await EmployeeModel.findOne({ email: req.body.email });
+  if (!employee) {
+    return next(
+      new ApiError('No such account exists, Try to reqister first...!', 404)
+    );
+  }
+  if (bcrypt.compareSync(req.body.password, employee.password)) {
+    let roleName = '';
 
-      const role = await RoleModel.findOne({ _id: employee.role });
-      if (role) {
-        roleName = role.name
-      }
-
-      let token = jwt.sign(
-        {id: employee._id, role: roleName},
-        process.env.JWT_SECRET,
-        {expiresIn: process.env.JWT_EXPIRES_IN}
-      )
-
-      res.status(200).json({
-        Message: 'Authenticated', 
-        token,
-        role: roleName
-      });
-      
-    }else{
-      return next(new ApiError("Invalid credentials, try to login again...!", 401))
+    const role = await RoleModel.findOne({ _id: employee.role });
+    if (role) {
+      roleName = role.name;
     }
+
+    const token = jwt.sign(
+      { id: employee._id, role: roleName },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      Message: 'Authenticated',
+      token,
+      role: roleName,
+    });
+  } else {
+    return next(
+      new ApiError('Invalid credentials, try to login again...!', 401)
+    );
   }
 });
 
 // Vendor Login ...
 exports.vendorLogin = asyncHandler(async (req, res, next) => {
-  const vendor = await VendorModel.findOne({ email: req.body.email});
-  if(!vendor) {
-    return next(new ApiError("No such account exists, Try to reqister first...!", 404))
-  }else{
-    if(bcrypt.compareSync(req.body.password, vendor.password)){
-      let roleName = ''
+  const vendor = await VendorModel.findOne({ email: req.body.email });
+  if (!vendor) {
+    return next(
+      new ApiError('No such account exists, Try to reqister first...!', 404)
+    );
+  }
+  if (bcrypt.compareSync(req.body.password, vendor.password)) {
+    let roleName = '';
 
-      const role = await RoleModel.findOne({ _id: vendor.role });
-      if (role) {
-        roleName = role.name
-      }
-
-      let token = jwt.sign(
-        {id: vendor._id, role: roleName},
-        process.env.JWT_SECRET,
-        {expiresIn: process.env.JWT_EXPIRES_IN}
-      )
-
-      res.status(200).json({
-        Message: 'Authenticated', 
-        token,
-        role: roleName
-      });
-    }else{
-      return next(new ApiError("Invalid credentials, try to login again...!", 401))
+    const role = await RoleModel.findOne({ _id: vendor.role });
+    if (role) {
+      roleName = role.name;
     }
+
+    const token = jwt.sign(
+      { id: vendor._id, role: roleName },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      Message: 'Authenticated',
+      token,
+      role: roleName,
+    });
+  } else {
+    return next(
+      new ApiError('Invalid credentials, try to login again...!', 401)
+    );
   }
 });
 
 // Customer Login ...
 exports.customerLogin = asyncHandler(async (req, res, next) => {
-  const customer = await customerModel.findOne({ email: req.body.email});
-  if(!customer) {
-    return next(new ApiError("No such account exists, Try to reqister first...!", 404))
-  }else{
-    if(bcrypt.compareSync(req.body.password, customer.password)){
-      let roleName = ''
+  const customer = await customerModel.findOne({ email: req.body.email });
+  if (!customer) {
+    return next(
+      new ApiError('No such account exists, Try to reqister first...!', 404)
+    );
+  }
+  if (bcrypt.compareSync(req.body.password, customer.password)) {
+    let roleName = '';
 
-      const role = await RoleModel.findOne({ _id: customer.role });
-      if (role) {
-        roleName = role.name
-      }
-
-      let token = jwt.sign(
-        {id: customer._id, role: roleName},
-        process.env.JWT_SECRET,
-        {expiresIn: process.env.JWT_EXPIRES_IN}
-      )
-
-      res.status(200).json({
-        Message: 'Authenticated', 
-        token,
-        role: roleName
-      });
-    }else{
-      return next(new ApiError("Invalid credentials, try to login again...!", 401))
+    const role = await RoleModel.findOne({ _id: customer.role });
+    if (role) {
+      roleName = role.name;
     }
+
+    const token = jwt.sign(
+      { id: customer._id, role: roleName },
+      process.env.JWT_SECRET,
+      { expiresIn: process.env.JWT_EXPIRES_IN }
+    );
+
+    res.status(200).json({
+      Message: 'Authenticated',
+      token,
+      role: roleName,
+    });
+  } else {
+    return next(
+      new ApiError('Invalid credentials, try to login again...!', 401)
+    );
   }
 });
