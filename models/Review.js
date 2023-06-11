3c44e064841b2e1b43e4fc18d294011b3d371e11
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-11 13:23:31 +0300

    Review Model Added

---
+++ models/Review.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const reviewSchema = new mongoose.Schema({
+  placeId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  userId: { type: mongoose.Schema.Types.ObjectId, required: true },
+  content: { type: String, required: true },
+  rating: { type: Number, required: true },
+  timestamp: { type: Date, default: Date.now },
+});
+
+const Review = mongoose.model('Review', reviewSchema);
+
+module.exports = Review;
