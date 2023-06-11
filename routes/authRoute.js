84fd5c2d3fc16ae1e6a070a3f12e88fc4187290a
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-12 01:01:06 +0300

    new forget pw routes added

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,28 +1,66 @@
 const express = require('express');
-const validatorMiddleware = require("../middlewares/validatorMiddleware");
-const {EmployeeOrAbove, VendorOrAbove, CustomerOrAbove} = require("../middlewares/authorizationMiddleware");
+const validatorMiddleware = require('../middlewares/validatorMiddleware');
+const {
+  EmployeeOrAbove,
+  VendorOrAbove,
+  CustomerOrAbove,
+} = require('../middlewares/authorizationMiddleware');
 
 const router = express.Router();
 const {
-  forgotPassword,
-  verifyPassResetCode,
-  resetPassword,
   vendorLogin,
   customerLogin,
   adminLogin,
 } = require('../controllers/authController');
 
 const {
+  customerForgotPassword,
+  customerVerifyPassResetCode,
+  customerResetPassword,
+} = require('../controllers/customerController');
+
+const {
+  vendorForgotPassword,
+  vendorVerifyPassResetCode,
+  vendorResetPassword,
+} = require('../controllers/vendorsController');
+
+const {
+  employeeForgotPassword,
+  employeeVerifyPassResetCode,
+  employeeResetPassword,
+} = require('../controllers/employeeController');
+
+const {
   forgotPasswordValidator,
   loginValidator,
 } = require('../utils/validators/authValidator');
 
-router.route('/employee/login').post(loginValidator, validatorMiddleware, adminLogin)
-router.route('/vendor/login').post(loginValidator, validatorMiddleware, vendorLogin)
-router.route('/customer/login').post(loginValidator, validatorMiddleware, customerLogin)
+router
+  .route('/employee/login')
+  .post(loginValidator, validatorMiddleware, adminLogin);
+router
+  .route('/vendor/login')
+  .post(loginValidator, validatorMiddleware, vendorLogin);
+router
+  .route('/customer/login')
+  .post(loginValidator, validatorMiddleware, customerLogin);
 
-router.route('/forgotPassword').post(forgotPasswordValidator, forgotPassword);
-router.route('/verifyResetCode').post(verifyPassResetCode);
-router.route('/resetPassword').put(resetPassword);
+router
+  .route('/customer/forgotPassword')
+  .post(forgotPasswordValidator, customerForgotPassword);
+router.route('/customer/verifyResetCode').post(customerVerifyPassResetCode);
+router.route('/customer/resetPassword').put(customerResetPassword);
 
+router
+  .route('/vendor/forgotPassword')
+  .post(forgotPasswordValidator, vendorForgotPassword);
+router.route('/vendor/verifyResetCode').post(vendorVerifyPassResetCode);
+router.route('/vendor/resetPassword').put(vendorResetPassword);
+
+router
+  .route('/employee/forgotPassword')
+  .post(forgotPasswordValidator, employeeForgotPassword);
+router.route('/employee/verifyResetCode').post(employeeVerifyPassResetCode);
+router.route('/employee/resetPassword').put(employeeResetPassword);
 module.exports = router;
