7adca673b2c023cffbc92187d1923bc45586b019
swe-amr-abdelaziz <swe.amr.abdelaziz@gmail.com>
2023-06-12 11:51:34 +0300

    Added basic updateCustomer

--- utils/validators/customerValidator.js
+++ utils/validators/customerValidator.js
@@ -7,6 +7,36 @@ exports.validatePostArray = [
   body("lastName").notEmpty().isAlpha().withMessage("Last name must be alphabetic"),
   body("email").notEmpty().isEmail().withMessage("Email is not valid"),
   body("password").notEmpty().isStrongPassword().withMessage("Password must be strong"),
+  body("street").isString().optional().withMessage("Street must be alphabetic"),
+  body("country").isString().optional().withMessage("Country must be alphabetic"),
+  body("state").isString().optional().withMessage("State must be alphabetic"),
+  body("city").isString().optional().withMessage("City must be alphabetic"),
+  body("zip").matches(/^(\d{5}(?:[-\s]\d{4})?)?$/).optional().withMessage("Zip must be numeric"),
+  body("phoneNumber").isString().optional().withMessage("Phone number is not valid"),
+  body("dateOfBirth")
+    .custom((value, { req }) => {
+      if (value === '') {
+        return true;
+      }
+      if (!value.match(/^\d{4}-\d{2}-\d{2}$/)) {
+        throw new Error('Date of birth must be a valid date');
+      }
+      return true;
+    })
+    .optional()
+    .withMessage("Date of birth must be a valid date"),
+  body("gender")
+    .optional()
+    .isIn(["male", "female"])
+    .withMessage("Gender must be either male or female"),
+  validatorMiddleware,
+];
+
+exports.validatePatchArray = [
+  body("firstName").optional().isAlpha().withMessage("First name must be alphabetic"),
+  body("lastName").optional().isAlpha().withMessage("Last name must be alphabetic"),
+  body("email").optional().isEmail().withMessage("Email is not valid"),
+  body("password").optional().isStrongPassword().withMessage("Password must be strong"),
   body("street").optional().isString().withMessage("Street must be alphabetic"),
   body("country").optional().isString().withMessage("Country must be alphabetic"),
   body("state").optional().isString().withMessage("State must be alphabetic"),
