ff0fc25ec6b83ebe4ed23621cf16f32e35f29c16
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-12 11:53:21 +0300

    progress committed

--- routes/contactUsRoute.js
+++ routes/contactUsRoute.js
@@ -1,19 +1,55 @@
 const express = require('express');
+const asyncHandler = require('express-async-handler');
+const ApiError = require('../utils/apiError');
+const sendMail = require('../utils/sendEmail');
 
 const router = express.Router();
-
-const { sendEmailToAdmin } = require('../controllers/contactUsController');
 const {
   contactUsValidator,
 } = require('../utils/validators/contactUsValidator');
 
 router.route('/').post(
   contactUsValidator,
-  (req, res, next) => {
-    console.log('hello');
-    next();
-  },
-  sendEmailToAdmin
+  asyncHandler(async (req, res, next) => {
+    console.log(req.body);
+
+    const { name, email, subject, message } = req.body;
+
+    const emailContent = `
+      <html>
+        <head>
+          <style>
+            /* Styles for the email content */
+          </style>
+        </head>
+        <body>
+          <div class="container">
+            <h1>New Contact Form Submission</h1>
+            <p>Name: ${name}</p>
+            <p>Email: ${email}</p>
+            <p>Subject: ${subject}</p>
+            <p>Message: ${message}</p>
+          </div>
+        </body>
+      </html>
+    `;
+    const adminEmail = 'mostafashaaban2080@gmail.com';
+    console.log(req.body);
+    try {
+      await sendMail({
+        email: adminEmail,
+        subject: 'New Contact Form Submission',
+        message: emailContent,
+      });
+    } catch (error) {
+      return next(new ApiError('Failed to send email to admin.', 400));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      message: 'Contact form submitted successfully.',
+    });
+  })
 );
 
 module.exports = router;
