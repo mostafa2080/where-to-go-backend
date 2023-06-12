d7514e3d8ace099648a5b27104ba902407c7d239
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-06-12 11:02:27 +0300

    modify routes

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -17,12 +17,13 @@ const {
   loginValidator,
 } = require('../utils/validators/authValidator');
 
+router.route('/forgotPassword').post(forgotPasswordValidator, forgotPassword);
+router.route('/verifyResetCode').post(verifyPassResetCode);
+router.route('/resetPassword').put(resetPassword);
+
 router.route('/employee/login').post(loginValidator, validatorMiddleware, adminLogin)
 router.route('/vendor/login').post(loginValidator, validatorMiddleware, vendorLogin)
 router.route('/customer/login').post(loginValidator, validatorMiddleware, customerLogin)
 
-router.route('/forgotPassword').post(forgotPasswordValidator, forgotPassword);
-router.route('/verifyResetCode').post(verifyPassResetCode);
-router.route('/resetPassword').put(resetPassword);
 
 module.exports = router;
