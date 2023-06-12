47de1e282094eb1cea86ab4829a2eb6b1cadf2e3
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-12 04:28:58 +0300

    fixed a category sent from body to categoryId to match controller

--- utils/validators/tagsValidator.js
+++ utils/validators/tagsValidator.js
@@ -13,7 +13,7 @@ exports.validateCreateTag = [
         .isLength({ max: 50 })
         .withMessage('Name must not exceed 50 characters'),
 
-    body('category').notEmpty().withMessage('Category Id is required'),
+    body('categoryId').notEmpty().withMessage('Category Id is required'),
 
     validatorMiddleware,
 ];
