0869de585732ce7d3aa8415632130cdc7bc4956b
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-14 01:08:39 +0300

    get/update/delete/updatePw logged vendor

--- controllers/vendorsController.js
+++ controllers/vendorsController.js
@@ -1,20 +1,27 @@
-const mongoose = require("mongoose");
-const asyncHandler = require("express-async-handler");
-const sharp = require("sharp");
-const { v4: uuidv4 } = require("uuid");
-require("../models/Vendor");
-require("../models/Tag");
-const path = require("path");
-const AsyncHandler = require("express-async-handler");
-const forgotPasswordController = require("./forgetPasswordController");
-const { uploadMixOfImages } = require("./imageController");
-const ApiError = require("../utils/apiError");
-const sendMail = require("../utils/sendEmail");
-const { resetPassword } = require("./forgetPasswordController");
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const asyncHandler = require('express-async-handler');
+const sharp = require('sharp');
+const { v4: uuidv4 } = require('uuid');
+require('../models/Vendor');
+require('../models/Tag');
+const path = require('path');
+const AsyncHandler = require('express-async-handler');
+const forgotPasswordController = require('./forgetPasswordController');
+const { uploadMixOfImages } = require('./imageController');
+const ApiError = require('../utils/apiError');
+const sendMail = require('../utils/sendEmail');
+const { resetPassword } = require('./forgetPasswordController');
 
-const Vendors = mongoose.model("vendor");
-const Roles = mongoose.model("roles");
-const Tags = mongoose.model("tag");
+const Vendors = mongoose.model('vendor');
+const Roles = mongoose.model('roles');
+const Tags = mongoose.model('tag');
+
+const createToken = (payload) =>
+  jwt.sign({ payload }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 const greetingMessage = asyncHandler(async (data) => {
   const emailContent = `
@@ -24,7 +31,7 @@ const greetingMessage = asyncHandler(async (data) => {
       </head>
       <body>
         <div class="container">
-          <h4>Wellcome ${data.firstName + " " + data.lastName} On Board </h4>
+          <h4>Wellcome ${data.firstName + ' ' + data.lastName} On Board </h4>
           <p>Congratlation for signing Up with ${data.email}  </p>
           <p>we 're sending this email to let you know that we have recieved your request for being a vendor </p>
           <p>and we will review your place details and within 24 - 48 Hours we will Respond </p>
@@ -35,7 +42,7 @@ const greetingMessage = asyncHandler(async (data) => {
   try {
     await sendMail({
       email: userEmail,
-      subject: "Greeting From Where To Go",
+      subject: 'Greeting From Where To Go',
       message: emailContent,
     });
   } catch (error) {
@@ -48,10 +55,10 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const skip = (page - 1) * limit;
   const sortField = req.query.sortField || null;
-  const sortOrder = req.query.sortOrder || "asc";
+  const sortOrder = req.query.sortOrder || 'asc';
   const filters = req.query.filters || {};
-  const searchQuery = req.query.search || "";
-  const tagIds = filters.tags ? filters.tags.split(",") : []; // Split the tagIds string into an array
+  const searchQuery = req.query.search || '';
+  const tagIds = filters.tags ? filters.tags.split(',') : []; // Split the tagIds string into an array
 
   const filterQuery = {};
 
@@ -66,15 +73,15 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
   // Apply search query to the filterQuery object
   if (searchQuery) {
     filterQuery.$or = [
-      { firstName: { $regex: searchQuery, $options: "i" } },
-      { lastName: { $regex: searchQuery, $options: "i" } },
-      { placeName: { $regex: searchQuery, $options: "i" } },
+      { firstName: { $regex: searchQuery, $options: 'i' } },
+      { lastName: { $regex: searchQuery, $options: 'i' } },
+      { placeName: { $regex: searchQuery, $options: 'i' } },
     ];
   }
 
   const sortQuery = {};
   if (sortField) {
-    sortQuery[sortField] = sortOrder === "desc" ? -1 : 1;
+    sortQuery[sortField] = sortOrder === 'desc' ? -1 : 1;
   }
 
   try {
@@ -94,14 +101,14 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
         .skip(skip)
         .limit(limit)
         .sort(sortQuery)
-        .populate("category"),
+        .populate('category'),
       Vendors.countDocuments(filterQuery),
     ]);
 
     const totalPages = Math.ceil(total / limit);
 
     return res.status(200).json({
-      status: "success",
+      status: 'success',
       pagination: {
         total,
         totalPages,
@@ -112,8 +119,8 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
     });
   } catch (error) {
     return res.status(500).json({
-      status: "error",
-      message: "Internal server error",
+      status: 'error',
+      message: 'Internal server error',
     });
   }
 });
@@ -121,7 +128,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
 exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: true });
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendors,
   });
 });
@@ -129,27 +136,27 @@ exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
 exports.getRejectedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: false });
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendors,
   });
 });
 
 exports.getVendor = AsyncHandler(async (req, res, next) => {
   const vendor = await Vendors.findById(req.params.id)
-    .populate("category")
+    .populate('category')
     .exec();
 
   const tags = await Tags.find({ _id: vendor.category._id });
   vendor.tags = tags;
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendor,
   });
 });
 
 exports.addVendor = asyncHandler(async (req, res, next) => {
-  const vendorRole = await Roles.find({ name: "Vendor" });
+  const vendorRole = await Roles.find({ name: 'Vendor' });
   req.body.role = vendorRole._id;
   const document = await Vendors.create(req.body);
   greetingMessage(document);
@@ -157,12 +164,12 @@ exports.addVendor = asyncHandler(async (req, res, next) => {
 });
 
 exports.updateVendor = asyncHandler(async (req, res, next) => {
-  console.log("updating");
+  console.log('updating');
   const document = await Vendors.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
   });
   if (!document) {
-    return next(new ApiError("Document not found", 404));
+    return next(new ApiError('Document not found', 404));
   }
   res.status(200).json({ data: document });
 });
@@ -179,7 +186,7 @@ exports.approveVendor = AsyncHandler(async (req, res, next) => {
     }
   );
   req.body.email = document.email;
-  req.body.modelType = "vendor";
+  req.body.modelType = 'vendor';
   next();
 });
 
@@ -197,11 +204,11 @@ exports.deactivateVendor = AsyncHandler(async (req, res, next) => {
 
   if (deletedVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: deletedVendor,
     });
   } else {
-    next(new Error("No Vendor With This Id"));
+    next(new Error('No Vendor With This Id'));
   }
 });
 
@@ -218,18 +225,18 @@ exports.restoreVendor = AsyncHandler(async (req, res, next) => {
   );
   if (restoredVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: restoredVendor,
     });
   } else {
-    next(new Error("No Vendor With This Id"));
+    next(new Error('No Vendor With This Id'));
   }
 });
 
 exports.uploadVendorImages = uploadMixOfImages([
-  { name: "thumbnail", maxCount: 1 },
+  { name: 'thumbnail', maxCount: 1 },
   {
-    name: "gallery",
+    name: 'gallery',
     maxCount: 5,
   },
 ]);
@@ -239,9 +246,9 @@ exports.processingImage = asyncHandler(async (req, res, next) => {
     const thumbnailFileName = `vendor-${uuidv4()}-${Date.now()}-cover.jpeg`;
     await sharp(req.files.thumbnail[0].buffer)
       .resize(2000, 1333)
-      .toFormat("jpeg")
+      .toFormat('jpeg')
       .jpeg({ quality: 90 })
-      .toFile(path.join(__dirname, "../images/vendors/", thumbnailFileName));
+      .toFile(path.join(__dirname, '../images/vendors/', thumbnailFileName));
     req.body.thumbnail = thumbnailFileName;
   }
   if (req.files && req.files.gallery) {
@@ -251,9 +258,9 @@ exports.processingImage = asyncHandler(async (req, res, next) => {
         const imageName = `vendor-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
         await sharp(img.buffer)
           .resize(2000, 1333)
-          .toFormat("jpeg")
+          .toFormat('jpeg')
           .jpeg({ quality: 90 })
-          .toFile(path.join(__dirname, "../images/vendors/", imageName));
+          .toFile(path.join(__dirname, '../images/vendors/', imageName));
 
         // save images to DB
         req.body.gallery.push(imageName);
@@ -269,3 +276,48 @@ exports.vendorVerifyPassResetCode =
   forgotPasswordController.verifyPassResetCode(Vendors);
 
 exports.vendorResetPassword = forgotPasswordController.resetPassword(Vendors);
+
+exports.getLoggedVendorData = AsyncHandler(async (req, res, next) => {
+  console.log(req.decodedToken.id);
+  req.params.id = req.decodedToken.id;
+  console.log(req.params.id);
+  next();
+});
+
+exports.updateLoggedVendorPassword = AsyncHandler(async (req, res, next) => {
+  //1) update user password based on the user payload (req.user._id)
+  const user = await Vendors.findByIdAndUpdate(
+    req.decodedToken.id,
+    {
+      password: await bcrypt.hash(req.body.password, 12),
+      passwordChangedAt: Date.now(),
+    },
+    {
+      new: true,
+    }
+  );
+  //2)generate token
+  const token = createToken(user._id);
+  res.status(200).json({
+    data: user,
+    token,
+  });
+});
+
+exports.updateLoggedVendorData = AsyncHandler(async (req, res, next) => {
+  const updatedUser = await Vendors.findByIdAndUpdate(
+    req.decodedToken.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+    },
+    { new: true }
+  );
+  res.status(200).json({ data: updatedUser });
+});
+
+exports.deleteLoggedVendorData = AsyncHandler(async (req, res, next) => {
+  await Vendors.findOneAndUpdate(req.decodedToken.id, { active: false });
+  res.status(200).json({ status: 'Your Account Deleted Successfully' });
+});
