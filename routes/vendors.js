b7c17037c58f5fad83ed79372d587c5430e62590
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-13 03:07:27 +0300

    uploading images and processing it added to routes

--- routes/vendors.js
+++ routes/vendors.js
@@ -1,30 +1,36 @@
-const express = require("express");
-const vendorsController = require("../controllers/vendorsController");
-const vendorValidator = require("../utils/validators/vendorValidator");
-const validatorMiddleware = require("../middlewares/validatorMiddleware");
-const { uploadImg, setImage } = require("../utils/imageUtility");
+const express = require('express');
+const vendorsController = require('../controllers/vendorsController');
+const vendorValidator = require('../utils/validators/vendorValidator');
+const validatorMiddleware = require('../middlewares/validatorMiddleware');
+const { uploadImg, setImage } = require('../utils/imageUtility');
 
 const router = express.Router();
 
 router
-  .route("/")
+  .route('/')
   .get(vendorsController.getAllVendors)
   .post(
-    uploadImg().fields([
-      { name: "thumbnail", maxCount: 1 },
-      { name: "gallery", maxCount: 10 },
-    ]),
+    vendorsController.uploadVendorImages,
+    (req, res, next) => {
+      console.log('upload', req.body);
+      next();
+    },
+    vendorsController.processingImage,
+    (req, res, next) => {
+      console.log('processing', req.body);
+      next();
+    },
     vendorValidator.addValidationArray,
     validatorMiddleware,
     vendorsController.addVendor
   );
 
-router.route("/approved").get(vendorsController.getApprovedVendors);
+router.route('/approved').get(vendorsController.getApprovedVendors);
 
-router.route("/rejected").get(vendorsController.getRejectedVendors);
+router.route('/rejected').get(vendorsController.getRejectedVendors);
 
 router
-  .route("/:id/deactivate")
+  .route('/:id/deactivate')
   .patch(
     vendorValidator.updateValidationArray,
     validatorMiddleware,
@@ -32,7 +38,7 @@ router
   );
 
 router
-  .route("/:id/restore")
+  .route('/:id/restore')
   .patch(
     vendorValidator.updateValidationArray,
     validatorMiddleware,
@@ -40,17 +46,23 @@ router
   );
 
 router
-  .route("/:id")
+  .route('/:id')
   .get(
     vendorValidator.getValidationArray,
     validatorMiddleware,
     vendorsController.getVendor
   )
-  .patch(
-    uploadImg().fields([
-      { name: "thumbnail", maxCount: 1 },
-      { name: "gallery", maxCount: 10 },
-    ]),
+  .put(
+    vendorsController.uploadVendorImages,
+    (req, res, next) => {
+      console.log('upload', req.body);
+      next();
+    },
+    vendorsController.processingImage,
+    (req, res, next) => {
+      console.log('processing', req.body);
+      next();
+    },
     vendorValidator.updateValidationArray,
     validatorMiddleware,
     vendorsController.updateVendor
