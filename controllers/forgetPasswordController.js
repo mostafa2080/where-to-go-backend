42fb7bb59213ba234d345e6d4e06b2ee98e7eae9
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-15 04:17:21 +0300

    fixed eslint errors in forgetPw controller

--- controllers/forgetPasswordController.js
+++ controllers/forgetPasswordController.js
@@ -1,21 +1,20 @@
-const mongoose = require("mongoose");
-const crypto = require("crypto");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const asyncHandler = require("express-async-handler");
-const ApiError = require("../utils/apiError");
-const sendMail = require("../utils/sendEmail");
-const { use } = require("../routes/imagesRouter");
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const asyncHandler = require('express-async-handler');
+const ApiError = require('../utils/apiError');
+const sendMail = require('../utils/sendEmail');
+const { use } = require('../routes/imagesRouter');
 
-require("../models/Customer");
-require("../models/Vendor");
-require("../models/Employee");
+require('../models/Customer');
+require('../models/Vendor');
+require('../models/Employee');
 
 const saltRunds = 10;
 const salt = bcrypt.genSaltSync(saltRunds);
 
-const forgetMessage = (user, resetCode) => {
-  return `
+const forgetMessage = (user, resetCode) => `
   <html>
     <head>
       <style>
@@ -63,22 +62,19 @@ const forgetMessage = (user, resetCode) => {
     </body>
   </html>
   `;
-};
-const approvalMessage = (user, resetCode) => {
-  return `
+const approvalMessage = (user, resetCode) => `
   <html>
     <head>
       <style>/* Styles for the email content */</style>
     </head>
     <body>
       <div class="container">
-        <h4>Congratlations Mr ${user.firstName + " " + user.lastName} </h4>
+        <h4>Congratlations Mr ${`${user.firstName} ${user.lastName}`} </h4>
         <p>Your Request For Being Vendor Has Been Approved</p>
         <p>Here is The Secret Key : ${resetCode}, To Assign Your Password</p>
       </div>
     </body>
   </html>`;
-};
 
 const createToken = (payload) =>
   jwt.sign({ payload }, process.env.JWT_SECRET, {
@@ -87,7 +83,7 @@ const createToken = (payload) =>
 
 exports.forgotPassword = (model) =>
   asyncHandler(async (req, res, next) => {
-    console.log("insde forget");
+    console.log('insde forget');
     //1) get user by email
     const user = await model.findOne({ email: req.body.email });
 
@@ -100,9 +96,9 @@ exports.forgotPassword = (model) =>
     //2) if email is exist, generate a hash reset random 6 digits and save it in DB
     const resetCode = Math.floor(100000 + Math.random() * 900000).toString();
     const hashedResetCode = crypto
-      .createHash("sha256")
+      .createHash('sha256')
       .update(resetCode)
-      .digest("hex");
+      .digest('hex');
 
     //save the hashed reset code into the database
     user.passwordResetToken = hashedResetCode;
@@ -121,7 +117,7 @@ exports.forgotPassword = (model) =>
     try {
       await sendMail({
         email: user.email,
-        subject: "Your Password reset code (Valid for 10 min )",
+        subject: 'Your Password reset code (Valid for 10 min )',
         message,
       });
     } catch (e) {
@@ -131,30 +127,30 @@ exports.forgotPassword = (model) =>
       await user.save();
       return next(
         new ApiError(
-          "Something went wrong when sending email, please try again later",
+          'Something went wrong when sending email, please try again later',
           500
         )
       );
     }
 
     res.status(200).json({
-      status: "success",
-      message: "Your reset code sent successfully",
+      status: 'success',
+      message: 'Your reset code sent successfully',
     });
   });
 
 //@desc verify reset code
 //@route POST /api/v1/auth/model/verifyResetCode
 //@access public
-let userMail = "";
+let userMail = '';
 
 exports.verifyPassResetCode = (model) =>
   asyncHandler(async (req, res, next) => {
     // 1) get user based on reset code
     const hashedCode = crypto
-      .createHash("sha256")
+      .createHash('sha256')
       .update(req.body.resetCode)
-      .digest("hex");
+      .digest('hex');
 
     const user = await model.findOne({
       passwordResetToken: hashedCode,
@@ -163,7 +159,7 @@ exports.verifyPassResetCode = (model) =>
 
     if (!user) {
       return next(
-        new ApiError("Password reset token is invalid or has expired", 400)
+        new ApiError('Password reset token is invalid or has expired', 400)
       );
     }
 
@@ -171,7 +167,7 @@ exports.verifyPassResetCode = (model) =>
     user.passwordResetVerified = true;
     await user.save();
 
-    res.status(200).json({ status: "success" });
+    res.status(200).json({ status: 'success' });
     userMail = user.email;
   });
 //@desc  reset pw
@@ -190,7 +186,7 @@ exports.resetPassword = (model) =>
 
     //2) check if reset code is verified
     if (!user.passwordResetVerified) {
-      return next(new ApiError("Password reset token not verified", 400));
+      return next(new ApiError('Password reset token not verified', 400));
     }
 
     user.password = bcrypt.hashSync(req.body.newPassword, salt);
