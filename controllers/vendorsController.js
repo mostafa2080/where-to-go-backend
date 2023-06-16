27b8311e4d8d4228466815047861beacd61f5cc4
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-16 13:18:32 +0300

    adding filtering by rating to vendor

--- controllers/vendorsController.js
+++ controllers/vendorsController.js
@@ -1,25 +1,27 @@
-const mongoose = require("mongoose");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const sharp = require("sharp");
-const io = require("socket.io-client");
-const { v4: uuidv4 } = require("uuid");
-const path = require("path");
-const AsyncHandler = require("express-async-handler");
-const forgotPasswordController = require("./forgetPasswordController");
-const { uploadMixOfImages } = require("./imageController");
-const ApiError = require("../utils/apiError");
-const sendMail = require("../utils/sendEmail");
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+const sharp = require('sharp');
+const io = require('socket.io-client');
+const { v4: uuidv4 } = require('uuid');
+const path = require('path');
+const AsyncHandler = require('express-async-handler');
+const forgotPasswordController = require('./forgetPasswordController');
+const { uploadMixOfImages } = require('./imageController');
+const ApiError = require('../utils/apiError');
+const sendMail = require('../utils/sendEmail');
 
-require("../models/Vendor");
-require("../models/Tag");
-require("../models/Category");
+require('../models/Vendor');
+require('../models/Tag');
+require('../models/Category');
 
-const socket = io("http://localhost:8001");
-const Vendors = mongoose.model("vendor");
-const Roles = mongoose.model("roles");
-const Tags = mongoose.model("tag");
-const Category = mongoose.model("category");
+const socket = io('http://localhost:8001');
+const Vendors = mongoose.model('vendor');
+const Roles = mongoose.model('roles');
+const Tags = mongoose.model('tag');
+const Category = mongoose.model('category');
+const Review = require('../models/Review');
+
 const createToken = (payload) =>
   jwt.sign({ payload }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
@@ -61,26 +63,149 @@ const greetingMessage = AsyncHandler(async (data) => {
   try {
     await sendMail({
       email: userEmail,
-      subject: "Greeting From Where To Go",
+      subject: 'Greeting From Where To Go',
       message: emailContent,
     });
   } catch (error) {
-    throw new ApiError("Sending Mail Failed Please Try Again.... ", 400);
+    throw new ApiError('Sending Mail Failed Please Try Again.... ', 400);
   }
 });
 
+// exports.getAllVendors = AsyncHandler(async (req, res, next) => {
+//   const page = parseInt(req.query.page, 10) || 1;
+//   const limit = parseInt(req.query.limit, 10) || 10;
+//   const skip = (page - 1) * limit;
+//   const sortField = req.query.sortField || null;
+//   const sortOrder = req.query.sortOrder || "asc";
+//   const filters = req.query.filters || {};
+//   const searchQuery = req.query.search || "";
+//   const categoryName = req.query.category || "";
+//   const tagSearchQuery = req.query.tags || "";
+
+//   const tagIds = filters.tags ? filters.tags.split(",") : [];
+
+//   const filterQuery = {};
+
+//   if (filters.isApproved !== undefined) {
+//     filterQuery.isApproved = filters.isApproved;
+//   }
+
+//   // Apply search query to the filterQuery object
+//   if (searchQuery) {
+//     filterQuery.$or = [
+//       { firstName: { $regex: searchQuery, $options: "i" } },
+//       { lastName: { $regex: searchQuery, $options: "i" } },
+//       { placeName: { $regex: searchQuery, $options: "i" } },
+//       { "address.country": { $regex: searchQuery, $options: "i" } },
+//       { "address.state": { $regex: searchQuery, $options: "i" } },
+//       { "address.city": { $regex: searchQuery, $options: "i" } },
+//       { "address.street": { $regex: searchQuery, $options: "i" } },
+//       { "address.zip": { $regex: searchQuery, $options: "i" } },
+//     ];
+//   }
+
+//   const sortQuery = {};
+//   if (sortField) {
+//     sortQuery[sortField] = sortOrder === "desc" ? -1 : 1;
+//   }
+
+//   try {
+//     // Find the tags that match the given tag IDs
+//     const tags = await Tags.find({ _id: { $in: tagIds } });
+
+//     // Extract the category IDs from the found tags
+//     const categoryIds = tags.map((tag) => tag.category);
+
+//     // Add the category IDs to the filter query
+//     if (categoryIds.length > 0) {
+//       filterQuery.category = { $in: categoryIds };
+//     }
+
+//     // Find the category by name
+//     if (categoryName) {
+//       const category = await Category.findOne({ name: categoryName });
+
+//       if (category) {
+//         filterQuery.category = category._id;
+//       } else {
+//         // Return an empty response if category not found
+//         return res.status(200).json({
+//           status: "success",
+//           pagination: {
+//             total: 0,
+//             totalPages: 0,
+//             currentPage: page,
+//             perPage: limit,
+//           },
+//           data: [],
+//         });
+//       }
+//     }
+
+//     // Find the vendors based on tag search query
+//     if (tagSearchQuery) {
+//       const matchingTags = await Tags.find({ name: tagSearchQuery });
+
+//       // Extract the category IDs from the found tags
+//       const matchingCategoryIds = matchingTags.map((tag) => tag.category);
+
+//       if (matchingCategoryIds.length > 0) {
+//         filterQuery.category = { $in: matchingCategoryIds };
+//       } else {
+//         // Return an empty response if no matching tags found
+//         return res.status(200).json({
+//           status: "success",
+//           pagination: {
+//             total: 0,
+//             totalPages: 0,
+//             currentPage: page,
+//             perPage: limit,
+//           },
+//           data: [],
+//         });
+//       }
+//     }
+
+//     const [vendors, total] = await Promise.all([
+//       Vendors.find(filterQuery)
+//         .skip(skip)
+//         .limit(limit)
+//         .sort(sortQuery)
+//         .populate("category"),
+//       Vendors.countDocuments(filterQuery),
+//     ]);
+
+//     const totalPages = Math.ceil(total / limit);
+
+//     return res.status(200).json({
+//       status: "success",
+//       pagination: {
+//         total,
+//         totalPages,
+//         currentPage: page,
+//         perPage: limit,
+//       },
+//       data: vendors,
+//     });
+//   } catch (error) {
+//     return res.status(500).json({
+//       status: "error",
+//       message: "Internal server error",
+//     });
+//   }
+// });
 exports.getAllVendors = AsyncHandler(async (req, res, next) => {
   const page = parseInt(req.query.page, 10) || 1;
   const limit = parseInt(req.query.limit, 10) || 10;
   const skip = (page - 1) * limit;
   const sortField = req.query.sortField || null;
-  const sortOrder = req.query.sortOrder || "asc";
+  const sortOrder = req.query.sortOrder || 'asc';
   const filters = req.query.filters || {};
-  const searchQuery = req.query.search || "";
-  const categoryName = req.query.category || "";
-  const tagSearchQuery = req.query.tags || "";
+  const searchQuery = req.query.search || '';
+  const categoryName = req.query.category || '';
+  const tagSearchQuery = req.query.tags || '';
 
-  const tagIds = filters.tags ? filters.tags.split(",") : [];
+  const tagIds = filters.tags ? filters.tags.split(',') : [];
 
   const filterQuery = {};
 
@@ -88,23 +213,38 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
     filterQuery.isApproved = filters.isApproved;
   }
 
+  // Add rating filter based on the Review collection
+  if (filters.rating) {
+    const rating = parseFloat(filters.rating);
+
+    if (!isNaN(rating)) {
+      const reviewFilter = { rating };
+
+      // Get the placeIds from the reviews with the specified rating
+      const reviewPlaceIds = await Review.distinct('placeId', reviewFilter);
+
+      // Add the filtered placeIds to the filterQuery
+      filterQuery._id = { $in: reviewPlaceIds };
+    }
+  }
+
   // Apply search query to the filterQuery object
   if (searchQuery) {
     filterQuery.$or = [
-      { firstName: { $regex: searchQuery, $options: "i" } },
-      { lastName: { $regex: searchQuery, $options: "i" } },
-      { placeName: { $regex: searchQuery, $options: "i" } },
-      { "address.country": { $regex: searchQuery, $options: "i" } },
-      { "address.state": { $regex: searchQuery, $options: "i" } },
-      { "address.city": { $regex: searchQuery, $options: "i" } },
-      { "address.street": { $regex: searchQuery, $options: "i" } },
-      { "address.zip": { $regex: searchQuery, $options: "i" } },
+      { firstName: { $regex: searchQuery, $options: 'i' } },
+      { lastName: { $regex: searchQuery, $options: 'i' } },
+      { placeName: { $regex: searchQuery, $options: 'i' } },
+      { 'address.country': { $regex: searchQuery, $options: 'i' } },
+      { 'address.state': { $regex: searchQuery, $options: 'i' } },
+      { 'address.city': { $regex: searchQuery, $options: 'i' } },
+      { 'address.street': { $regex: searchQuery, $options: 'i' } },
+      { 'address.zip': { $regex: searchQuery, $options: 'i' } },
     ];
   }
 
   const sortQuery = {};
   if (sortField) {
-    sortQuery[sortField] = sortOrder === "desc" ? -1 : 1;
+    sortQuery[sortField] = sortOrder === 'desc' ? -1 : 1;
   }
 
   try {
@@ -128,7 +268,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
       } else {
         // Return an empty response if category not found
         return res.status(200).json({
-          status: "success",
+          status: 'success',
           pagination: {
             total: 0,
             totalPages: 0,
@@ -152,7 +292,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
       } else {
         // Return an empty response if no matching tags found
         return res.status(200).json({
-          status: "success",
+          status: 'success',
           pagination: {
             total: 0,
             totalPages: 0,
@@ -169,14 +309,14 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
         .skip(skip)
         .limit(limit)
         .sort(sortQuery)
-        .populate("category"),
+        .populate('category'),
       Vendors.countDocuments(filterQuery),
     ]);
 
     const totalPages = Math.ceil(total / limit);
 
     return res.status(200).json({
-      status: "success",
+      status: 'success',
       pagination: {
         total,
         totalPages,
@@ -187,8 +327,8 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
     });
   } catch (error) {
     return res.status(500).json({
-      status: "error",
-      message: "Internal server error",
+      status: 'error',
+      message: 'Internal server error',
     });
   }
 });
@@ -196,7 +336,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
 exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: true });
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendors,
   });
 });
@@ -204,21 +344,21 @@ exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
 exports.getRejectedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: false });
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendors,
   });
 });
 
 exports.getVendor = AsyncHandler(async (req, res, next) => {
   const vendor = await Vendors.findById(req.params.id)
-    .populate("category")
+    .populate('category')
     .exec();
 
   const tags = await Tags.find({ category: vendor.category[0]._id });
   vendor.tags = tags;
 
   res.status(200).json({
-    status: "success",
+    status: 'success',
     data: vendor,
     tags: tags,
   });
@@ -230,11 +370,11 @@ exports.addVendor = AsyncHandler(async (req, res, next) => {
     state: req.body.state,
     city: req.body.city,
     zip: +req.body.zip,
-    street: req.body.street || "st",
+    street: req.body.street || 'st',
   };
   req.body.address = address;
 
-  const vendorRole = await Roles.find({ name: "Vendor" });
+  const vendorRole = await Roles.find({ name: 'Vendor' });
   req.body.role = vendorRole._id;
 
   // const saltRunds = 10;
@@ -245,7 +385,7 @@ exports.addVendor = AsyncHandler(async (req, res, next) => {
   const document = await Vendors.create(req.body);
   greetingMessage(document);
   const message = `A new request for Adding New Place Named ${document.placeName} For Mr ${document.firstName} ${document.lastName} `;
-  socket.emit("notifyAdminAndEmpForAddingVendor", message);
+  socket.emit('notifyAdminAndEmpForAddingVendor', message);
 
   res.status(201).json({ data: document });
 });
@@ -255,7 +395,7 @@ exports.updateVendor = AsyncHandler(async (req, res, next) => {
     new: true,
   });
   if (!document) {
-    return next(new ApiError("Document not found", 404));
+    return next(new ApiError('Document not found', 404));
   }
   res.status(200).json({ data: document });
 });
@@ -272,10 +412,10 @@ exports.approveVendor = AsyncHandler(async (req, res, next) => {
     }
   );
   if (!document) {
-    return next(new ApiError("No Vendor Found", 404));
+    return next(new ApiError('No Vendor Found', 404));
   }
   req.body.email = document.email;
-  req.body.modelType = "vendor";
+  req.body.modelType = 'vendor';
   next();
 });
 
@@ -293,11 +433,11 @@ exports.deactivateVendor = AsyncHandler(async (req, res, next) => {
 
   if (deletedVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: deletedVendor,
     });
   } else {
-    next(new Error("No Vendor With This Id"));
+    next(new Error('No Vendor With This Id'));
   }
 });
 
@@ -314,18 +454,18 @@ exports.restoreVendor = AsyncHandler(async (req, res, next) => {
   );
   if (restoredVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: "success",
+      status: 'success',
       data: restoredVendor,
     });
   } else {
-    next(new Error("No Vendor With This Id"));
+    next(new Error('No Vendor With This Id'));
   }
 });
 
 exports.uploadVendorImages = uploadMixOfImages([
-  { name: "thumbnail", maxCount: 1 },
+  { name: 'thumbnail', maxCount: 1 },
   {
-    name: "gallery",
+    name: 'gallery',
     maxCount: 5,
   },
 ]);
@@ -335,9 +475,9 @@ exports.processingImage = AsyncHandler(async (req, res, next) => {
     const thumbnailFileName = `vendor-${uuidv4()}-${Date.now()}-cover.jpeg`;
     await sharp(req.files.thumbnail[0].buffer)
       .resize(2000, 1333)
-      .toFormat("jpeg")
+      .toFormat('jpeg')
       .jpeg({ quality: 90 })
-      .toFile(path.join(__dirname, "../images/vendors/", thumbnailFileName));
+      .toFile(path.join(__dirname, '../images/vendors/', thumbnailFileName));
     req.body.thumbnail = thumbnailFileName;
   }
   if (req.files && req.files.gallery) {
@@ -347,9 +487,9 @@ exports.processingImage = AsyncHandler(async (req, res, next) => {
         const imageName = `vendor-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
         await sharp(img.buffer)
           .resize(2000, 1333)
-          .toFormat("jpeg")
+          .toFormat('jpeg')
           .jpeg({ quality: 90 })
-          .toFile(path.join(__dirname, "../images/vendors/", imageName));
+          .toFile(path.join(__dirname, '../images/vendors/', imageName));
 
         // save images to DB
         req.body.gallery.push(imageName);
@@ -408,5 +548,5 @@ exports.updateLoggedVendorData = AsyncHandler(async (req, res, next) => {
 
 exports.deleteLoggedVendorData = AsyncHandler(async (req, res, next) => {
   await Vendors.findOneAndUpdate(req.decodedToken.id, { active: false });
-  res.status(200).json({ status: "Your Account Deleted Successfully" });
+  res.status(200).json({ status: 'Your Account Deleted Successfully' });
 });
