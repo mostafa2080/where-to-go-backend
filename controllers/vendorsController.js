90ce540ab517b98a3d5b3e587e292ce270edaff2
Youssef Abdullah <ZeroCrashOverride097@gmail.com>
2023-06-17 08:06:10 +0300

    saving images after saving vendor data in database

--- controllers/vendorsController.js
+++ controllers/vendorsController.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose');
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const sharp = require('sharp');
-const io = require('socket.io-client');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const AsyncHandler = require('express-async-handler');
-const forgotPasswordController = require('./forgetPasswordController');
-const { uploadMixOfImages } = require('./imageController');
-const ApiError = require('../utils/apiError');
-const sendMail = require('../utils/sendEmail');
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const sharp = require("sharp");
+const io = require("socket.io-client");
+const { v4: uuidv4 } = require("uuid");
+const path = require("path");
+const AsyncHandler = require("express-async-handler");
+const forgotPasswordController = require("./forgetPasswordController");
+const { uploadMixOfImages } = require("./imageController");
+const ApiError = require("../utils/apiError");
+const sendMail = require("../utils/sendEmail");
 
-require('../models/Vendor');
-require('../models/Tag');
-require('../models/Category');
+require("../models/Vendor");
+require("../models/Tag");
+require("../models/Category");
 
-const socket = io('http://localhost:8001');
-const Vendors = mongoose.model('vendor');
-const Roles = mongoose.model('roles');
-const Tags = mongoose.model('tag');
-const Category = mongoose.model('category');
-const Review = require('../models/Review');
+const socket = io("http://localhost:8001");
+const Vendors = mongoose.model("vendor");
+const Roles = mongoose.model("roles");
+const Tags = mongoose.model("tag");
+const Category = mongoose.model("category");
+const Review = require("../models/Review");
 
 const createToken = (payload) =>
   jwt.sign({ payload }, process.env.JWT_SECRET, {
@@ -63,11 +63,11 @@ const greetingMessage = AsyncHandler(async (data) => {
   try {
     await sendMail({
       email: userEmail,
-      subject: 'Greeting From Where To Go',
+      subject: "Greeting From Where To Go",
       message: emailContent,
     });
   } catch (error) {
-    throw new ApiError('Sending Mail Failed Please Try Again.... ', 400);
+    throw new ApiError("Sending Mail Failed Please Try Again.... ", 400);
   }
 });
 
@@ -76,13 +76,13 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
   const limit = parseInt(req.query.limit, 10) || 10;
   const skip = (page - 1) * limit;
   const sortField = req.query.sortField || null;
-  const sortOrder = req.query.sortOrder || 'asc';
+  const sortOrder = req.query.sortOrder || "asc";
   const filters = req.query.filters || {};
-  const searchQuery = req.query.search || '';
-  const categoryName = req.query.category || '';
-  const tagSearchQuery = req.query.tags || '';
+  const searchQuery = req.query.search || "";
+  const categoryName = req.query.category || "";
+  const tagSearchQuery = req.query.tags || "";
 
-  const tagIds = filters.tags ? filters.tags.split(',') : [];
+  const tagIds = filters.tags ? filters.tags.split(",") : [];
 
   const filterQuery = {};
 
@@ -98,7 +98,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
       const reviewFilter = { rating };
 
       // Get the placeIds from the reviews with the specified rating
-      const reviewPlaceIds = await Review.distinct('placeId', reviewFilter);
+      const reviewPlaceIds = await Review.distinct("placeId", reviewFilter);
 
       // Add the filtered placeIds to the filterQuery
       filterQuery._id = { $in: reviewPlaceIds };
@@ -108,20 +108,20 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
   // Apply search query to the filterQuery object
   if (searchQuery) {
     filterQuery.$or = [
-      { firstName: { $regex: searchQuery, $options: 'i' } },
-      { lastName: { $regex: searchQuery, $options: 'i' } },
-      { placeName: { $regex: searchQuery, $options: 'i' } },
-      { 'address.country': { $regex: searchQuery, $options: 'i' } },
-      { 'address.state': { $regex: searchQuery, $options: 'i' } },
-      { 'address.city': { $regex: searchQuery, $options: 'i' } },
-      { 'address.street': { $regex: searchQuery, $options: 'i' } },
-      { 'address.zip': { $regex: searchQuery, $options: 'i' } },
+      { firstName: { $regex: searchQuery, $options: "i" } },
+      { lastName: { $regex: searchQuery, $options: "i" } },
+      { placeName: { $regex: searchQuery, $options: "i" } },
+      { "address.country": { $regex: searchQuery, $options: "i" } },
+      { "address.state": { $regex: searchQuery, $options: "i" } },
+      { "address.city": { $regex: searchQuery, $options: "i" } },
+      { "address.street": { $regex: searchQuery, $options: "i" } },
+      { "address.zip": { $regex: searchQuery, $options: "i" } },
     ];
   }
 
   const sortQuery = {};
   if (sortField) {
-    sortQuery[sortField] = sortOrder === 'desc' ? -1 : 1;
+    sortQuery[sortField] = sortOrder === "desc" ? -1 : 1;
   }
 
   try {
@@ -150,7 +150,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
       } else {
         // Return an empty response if category not found
         return res.status(200).json({
-          status: 'success',
+          status: "success",
           pagination: {
             total: 0,
             totalPages: 0,
@@ -174,7 +174,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
       } else {
         // Return an empty response if no matching tags found
         return res.status(200).json({
-          status: 'success',
+          status: "success",
           pagination: {
             total: 0,
             totalPages: 0,
@@ -191,14 +191,14 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
         .skip(skip)
         .limit(limit)
         .sort(sortQuery)
-        .populate('category'),
+        .populate("category"),
       Vendors.countDocuments(filterQuery),
     ]);
 
     const totalPages = Math.ceil(total / limit);
 
     return res.status(200).json({
-      status: 'success',
+      status: "success",
       pagination: {
         total,
         totalPages,
@@ -209,8 +209,8 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
     });
   } catch (error) {
     return res.status(500).json({
-      status: 'error',
-      message: 'Internal server error',
+      status: "error",
+      message: "Internal server error",
     });
   }
 });
@@ -218,7 +218,7 @@ exports.getAllVendors = AsyncHandler(async (req, res, next) => {
 exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: true });
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: vendors,
   });
 });
@@ -226,24 +226,42 @@ exports.getApprovedVendors = AsyncHandler(async (req, res, next) => {
 exports.getRejectedVendors = AsyncHandler(async (req, res, next) => {
   const vendors = await Vendors.find({ isApproved: false });
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: vendors,
   });
 });
 
 exports.getVendor = AsyncHandler(async (req, res, next) => {
   const vendor = await Vendors.findById(req.params.id)
-    .populate('category')
+    .populate("category")
     .exec();
 
   const tags = await Tags.find({ category: vendor.category[0]._id });
   vendor.tags = tags;
 
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: vendor,
     tags: tags,
   });
+});
+
+exports.updatingDatabaseImageValues = AsyncHandler(async (req, res, next) => {
+  if (req.files && req.files.thumbnail) {
+    req.body.thumbnail = `vendor-${uuidv4()}-${Date.now()}-cover.jpeg`;
+  }
+
+  if (req.files && req.files.gallery) {
+    req.body.gallery = [];
+    await Promise.all(
+      req.files.gallery.map(async (img, index) => {
+        req.body.gallery.push(
+          `vendor-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`
+        );
+      })
+    );
+  }
+  next();
 });
 
 exports.addVendor = AsyncHandler(async (req, res, next) => {
@@ -252,23 +270,45 @@ exports.addVendor = AsyncHandler(async (req, res, next) => {
     state: req.body.state,
     city: req.body.city,
     zip: +req.body.zip,
-    street: req.body.street || 'st',
+    street: req.body.street || "st",
   };
   req.body.address = address;
 
-  const vendorRole = await Roles.find({ name: 'Vendor' });
+  const vendorRole = await Roles.find({ name: "Vendor" });
   req.body.role = vendorRole._id;
 
   const saltRunds = 10;
   const salt = bcrypt.genSaltSync(saltRunds);
-  const passwordBody = req.body.password || '';
+  const passwordBody = req.body.password || "";
   const password = bcrypt.hashSync(passwordBody, salt);
   req.body.password = password;
 
   const document = await Vendors.create(req.body);
+
+  if (req.files && req.files.thumbnail) {
+    await sharp(req.files.thumbnail[0].buffer)
+      .resize(2000, 1333)
+      .toFormat("jpeg")
+      .jpeg({ quality: 90 })
+      .toFile(path.join(__dirname, "../images/vendors/", req.body.thumbnail));
+  }
+  if (req.files && req.files.gallery) {
+    await Promise.all(
+      req.files.gallery.map(async (img, index) => {
+        await sharp(img.buffer)
+          .resize(2000, 1333)
+          .toFormat("jpeg")
+          .jpeg({ quality: 90 })
+          .toFile(
+            path.join(__dirname, "../images/vendors/", req.body.gallery[index])
+          );
+      })
+    );
+  }
+
   greetingMessage(document);
   const message = `A new request for Adding New Place Named ${document.placeName} For Mr ${document.firstName} ${document.lastName} `;
-  socket.emit('notifyAdminAndEmpForAddingVendor', message);
+  socket.emit("notifyAdminAndEmpForAddingVendor", message);
 
   res.status(201).json({ data: document });
 });
@@ -278,7 +318,7 @@ exports.updateVendor = AsyncHandler(async (req, res, next) => {
     new: true,
   });
   if (!document) {
-    return next(new ApiError('Document not found', 404));
+    return next(new ApiError("Document not found", 404));
   }
   res.status(200).json({ data: document });
 });
@@ -295,10 +335,10 @@ exports.approveVendor = AsyncHandler(async (req, res, next) => {
     }
   );
   if (!document) {
-    return next(new ApiError('No Vendor Found', 404));
+    return next(new ApiError("No Vendor Found", 404));
   }
   req.body.email = document.email;
-  req.body.modelType = 'vendor';
+  req.body.modelType = "vendor";
   next();
 });
 
@@ -316,11 +356,11 @@ exports.deactivateVendor = AsyncHandler(async (req, res, next) => {
 
   if (deletedVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: deletedVendor,
     });
   } else {
-    next(new Error('No Vendor With This Id'));
+    next(new Error("No Vendor With This Id"));
   }
 });
 
@@ -337,18 +377,18 @@ exports.restoreVendor = AsyncHandler(async (req, res, next) => {
   );
   if (restoredVendor.modifiedCount > 0) {
     res.status(200).json({
-      status: 'success',
+      status: "success",
       data: restoredVendor,
     });
   } else {
-    next(new Error('No Vendor With This Id'));
+    next(new Error("No Vendor With This Id"));
   }
 });
 
 exports.uploadVendorImages = uploadMixOfImages([
-  { name: 'thumbnail', maxCount: 1 },
+  { name: "thumbnail", maxCount: 1 },
   {
-    name: 'gallery',
+    name: "gallery",
     maxCount: 5,
   },
 ]);
@@ -358,9 +398,9 @@ exports.processingImage = AsyncHandler(async (req, res, next) => {
     const thumbnailFileName = `vendor-${uuidv4()}-${Date.now()}-cover.jpeg`;
     await sharp(req.files.thumbnail[0].buffer)
       .resize(2000, 1333)
-      .toFormat('jpeg')
+      .toFormat("jpeg")
       .jpeg({ quality: 90 })
-      .toFile(path.join(__dirname, '../images/vendors/', thumbnailFileName));
+      .toFile(path.join(__dirname, "../images/vendors/", thumbnailFileName));
     req.body.thumbnail = thumbnailFileName;
   }
   if (req.files && req.files.gallery) {
@@ -370,9 +410,9 @@ exports.processingImage = AsyncHandler(async (req, res, next) => {
         const imageName = `vendor-${uuidv4()}-${Date.now()}-${index + 1}.jpeg`;
         await sharp(img.buffer)
           .resize(2000, 1333)
-          .toFormat('jpeg')
+          .toFormat("jpeg")
           .jpeg({ quality: 90 })
-          .toFile(path.join(__dirname, '../images/vendors/', imageName));
+          .toFile(path.join(__dirname, "../images/vendors/", imageName));
 
         // save images to DB
         req.body.gallery.push(imageName);
@@ -431,5 +471,5 @@ exports.updateLoggedVendorData = AsyncHandler(async (req, res, next) => {
 
 exports.deleteLoggedVendorData = AsyncHandler(async (req, res, next) => {
   await Vendors.findOneAndUpdate(req.decodedToken.id, { active: false });
-  res.status(200).json({ status: 'Your Account Deleted Successfully' });
+  res.status(200).json({ status: "Your Account Deleted Successfully" });
 });
