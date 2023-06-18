8c2aa1aee8244f0a944a0b983d6abeaf99f22182
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-19 00:42:06 +0300

    modifying validation of contactUs

--- utils/validators/contactUsValidator.js
+++ utils/validators/contactUsValidator.js
@@ -1,14 +1,14 @@
-const { body } = require("express-validator");
-const validatorMiddleware = require("../../middlewares/validatorMiddleware");
+const { body } = require('express-validator');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
 
 exports.contactUsValidator = [
-  body("name")
+  body('name')
     .notEmpty()
     .withMessage("Name Can't Be Empty")
-    .isAlpha()
-    .withMessage("Name Must Be Alphabetic"),
-  body("email").isEmail().withMessage("Email Must Be Valid Email"),
-  //body('subject').notEmpty().withMessage('Subject is required.'),
-  body("message").notEmpty().withMessage("Message Can't Be Empty."),
+    .isString()
+    .withMessage('Name Must Be Alphabetic'),
+  body('email').isEmail().withMessage('Email Must Be Valid Email'),
+  body('subject').notEmpty().withMessage('Subject is required.'),
+  body('message').notEmpty().withMessage("Message Can't Be Empty."),
   validatorMiddleware,
 ];
