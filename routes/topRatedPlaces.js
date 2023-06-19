2c8fe1587d2d2b1c592cb60938a2b461655072f8
Muhammed-saber79 <muhammed.saber385@gmail.com>
2023-06-19 16:30:53 +0300

    remove topRated places file from routes

--- routes/topRatedPlaces.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const vendorsController = require("../controllers/vendorsController");
-
-const router = express.Router();
-
-router.get("/", vendorsController.getTopRatedPlaces);
-
-module.exports = router;
