90d4d3d78fa0076cd8f6e0f189ee3df2474683ab
mostafa2080 <mostafashaaban2080@gmail.com>
2023-06-21 14:52:44 +0300

    minor changes in review validator

--- utils/validators/reviewValidator.js
+++ utils/validators/reviewValidator.js
@@ -1,62 +1,68 @@
-const { body, param } = require("express-validator");
-const validatorMiddleware = require("../../middlewares/validatorMiddleware");
-const User = require("../../models/Customer");
-const ApiError = require("../apiError");
-const Place = require("../../models/Vendor");
-const Review = require("../../models/Review");
+const { body, param } = require('express-validator');
+const validatorMiddleware = require('../../middlewares/validatorMiddleware');
+const User = require('../../models/Customer');
+const ApiError = require('../apiError');
+const Place = require('../../models/Vendor');
+const Review = require('../../models/Review');
 
 // Validation for createReview
 exports.validateCreateReview = [
-  body("placeId").custom(async (value) => {
+  body('placeId').custom(async (value) => {
     const place = await Place.findById(value);
     if (!place) {
-      throw new ApiError("Invalid place ID", 404);
+      throw new ApiError('Invalid place ID', 404);
     }
     return true;
   }),
 
-  body("userId").custom(async (value, { req }) => {
+  body('userId').custom(async (value, { req }) => {
     console.log(req.decodedToken.payload.id);
     const user = await User.findById(req.decodedToken.payload.id);
     if (!user) {
-      throw new ApiError("Invalid user ID", 404);
+      throw new ApiError('Invalid user ID', 404);
+    }
+    if (req.decodedToken.payload.id === 'vendor') {
+      throw new ApiError(
+        'As a Vendor your are not allowed to perform this action ',
+        404
+      );
     }
     return true;
   }),
 
-  body("content")
+  body('content')
     .trim()
     .notEmpty()
-    .withMessage("Review content is required")
+    .withMessage('Review content is required')
     .isLength({ max: 500 })
-    .withMessage("Review content must not exceed 500 characters"),
+    .withMessage('Review content must not exceed 500 characters'),
 
-  body("rating")
+  body('rating')
     .isFloat({ min: 1, max: 5 })
-    .withMessage("Rating Value must be  between 1 and 5"),
+    .withMessage('Rating Value must be  between 1 and 5'),
 
   validatorMiddleware,
 ];
 
 // Validation for updateReview
 exports.validateUpdateReview = [
-  param("id")
+  param('id')
     .isMongoId()
-    .withMessage("Cannot find review with this id")
+    .withMessage('Cannot find review with this id')
     .custom((value, { req }) => {
       console.log(req.decodedToken.payload.role);
       return new Promise((resolve, reject) => {
         Review.findById(value)
           .then((review) => {
             if (!review) {
-              const error = new ApiError("No Review Found for this ID", 404);
+              const error = new ApiError('No Review Found for this ID', 404);
               reject(error);
             } else if (
               review.userId.toString() !==
               req.decodedToken.payload.id.toString()
             ) {
               const error = new ApiError(
-                "You do not have the right to perform this action",
+                'You do not have the right to perform this action',
                 400
               );
               reject(error);
@@ -69,39 +75,39 @@ exports.validateUpdateReview = [
           });
       });
     }),
-  body("content")
+  body('content')
     .optional()
     .trim()
     .isLength({ max: 500 })
-    .withMessage("Review content must not exceed 500 characters"),
+    .withMessage('Review content must not exceed 500 characters'),
 
-  body("rating")
+  body('rating')
     .optional()
     .isFloat({ min: 1, max: 5 })
-    .withMessage("Rating must be a number between 1 and 5"),
+    .withMessage('Rating must be a number between 1 and 5'),
 
   validatorMiddleware,
 ];
 
 // Validation for deleteReview
 exports.validateDeleteReview = [
-  param("id")
+  param('id')
     .isMongoId()
-    .withMessage("Invalid review id format")
+    .withMessage('Invalid review id format')
     .custom((value, { req }) => {
-      if (req.decodedToken.payload.role === "Customer") {
+      if (req.decodedToken.payload.role === 'Customer') {
         return new Promise((resolve, reject) => {
           Review.findById(value)
             .then((review) => {
               if (!review) {
-                const error = new ApiError("No Review Found for this ID", 404);
+                const error = new ApiError('No Review Found for this ID', 404);
                 reject(error);
               } else if (
                 review.userId.toString() !==
                 req.decodedToken.payload.id.toString()
               ) {
                 const error = new ApiError(
-                  "You do not have the right to perform this action",
+                  'You do not have the right to perform this action',
                   400
                 );
                 reject(error);
@@ -114,10 +120,10 @@ exports.validateDeleteReview = [
             });
         });
       }
-      if (req.decodedToken.payload.role === "vendor") {
+      if (req.decodedToken.payload.role === 'vendor') {
         console.log(req.decodedToken.payload.role);
         const error = new ApiError(
-          "As a Vendor, you do not have the right to perform this action",
+          'As a Vendor, you do not have the right to perform this action',
           400
         );
         return Promise.reject(error);
